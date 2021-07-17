ba3388d1877f1bba2ef42d304c32ae08f8d8c4b4
padmavathi2001 <87537899+padmavathi2001@users.noreply.github.com>
2021-07-17 07:28:42 +0530

    Download Files

---
+++ mobf.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Display</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+
+     <?php 
+
+
+
+include 'db_conn.php';
+include 'ind.php';
+
+$query = "SELECT * FROM mob ORDER by id DESC";
+          $res = mysqli_query($conn,  $query);
+?>
+
+<br>
+          <table>
+          <tr>
+              <th>Topic Name</th>
+              <th>Download</th>
+          </tr>
+
+<?php
+
+          if (mysqli_num_rows($res) > 0) {
+      ?>        
+
+  <?php
+          	while ($row = mysqli_fetch_assoc($res)) {  ?>
+             
+             <tr>
+               
+               <td><?php echo $row['name'];?></td>
+<td>
+<button><a href="uploads/<?=$row['file_url']?>">Download</a></button>
+</td>
+
+           </tr>
+
+
+
+          		
+    <?php } 
+  
+  }?>
+			  
+</body>
+</html>
