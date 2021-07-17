b6b6087c9a1e72988c885c722cc33f8a38828178
padmavathi2001 <87537899+padmavathi2001@users.noreply.github.com>
2021-07-17 07:32:28 +0530

    Index File

---
+++ index.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html>
+  <?php
+include 'ind.php';
+  ?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+  
+  background-image:url('co.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background:  green;
+   color: white;
+   text-align: center;
+   font-size:20px;
+}
+
+h2 {
+    text-align: center;
+    color:white;
+    font-size:32px;
+    background:blue;
+    font-weight: "bold";
+   }
+
+h3 {
+    text-align: center;
+    color:white;
+    background-color:blue;
+    font-size:30px;
+    font-weight: "bold"; 
+   }
+   footer
+   {
+     text-align:last;
+     justify-content:center;
+     background:blue;
+     font-size:20px;
+     color:white;
+
+   }
+</style>
+</head>
+<body>
+
+<h2>Welocme to E-Books website</h2>
+<h3>Click on open to download your branch E-books and materials for free</h3>
+
+<div class="footer">
+  <p>Developed by Padmavathi</p>
+</div>
+
+</body>
+</html> 
