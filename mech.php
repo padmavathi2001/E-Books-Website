af39a16dcf1aaa6df1d7691563dcb698e60427e5
padmavathi2001 <87537899+padmavathi2001@users.noreply.github.com>
2021-07-17 07:31:46 +0530

    MECH E-Books

---
+++ mech.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+
+include 'ind.php';
+?>
+
+<style>
+    body {
+    font-size: 16px;
+    background: pink;
+    font-family: "Segoe UI", "Helvetica Neue", Arial, sans-serif;
+    background-image:url('co.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+h2 {
+    text-align: center;
+    color:mintcream;
+    font-weight: "bold";
+   }
+
+h3 {
+    text-align: center;
+    color:navy;
+    font-weight: "bold"; 
+   }
+
+div.gallery {
+  margin: 10px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 300px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width:300px;
+  height:350px;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+
+</style>
+</head>
+<body>
+
+
+<h2>Welocme to E-Books website</h2>
+<h3>Download Mech branch books for free</h3>
+<div class="gallery">
+  
+<a href="mech/m1.pdf">
+    <img alt="" src="mech/m1.jpg">
+  </a>
+  
+</div>
+
+<div class="gallery">
+   
+<a href="mech/m2.pdf">
+    <img alt="" src="mech/m2.jpg">
+  </a>
+  
+</div>
+
+<div class="gallery">
+  <a href="mech/m3.pdf">
+    <img alt="" src="mech/m3.jpg">
+  </a>
+
+</div>
+
+
+<div class="gallery">
+  <a href="mech/m4.pdf">
+    <img alt="" src="mech/m4.jpg">
+  </a>
+
+</div>
+
+
+
+
+</body>
+</html>
