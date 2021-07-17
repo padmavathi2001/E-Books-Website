1a78579fc2338d5ae21b9a5c71712e2cfab34993
padmavathi2001 <87537899+padmavathi2001@users.noreply.github.com>
2021-07-17 07:30:01 +0530

    Upload file(Only admin can do)

---
+++ cse.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+
+include 'ind.php';
+?>
+
+<style>
+    body {
+    font-size: 16px;
+    background: pink;
+    font-family: "Segoe UI", "Helvetica Neue", Arial, sans-serif;
+    background-image:url('co.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+h2 {
+    text-align: center;
+    color:mintcream;
+    font-weight: "bold";
+   }
+
+h3 {
+    text-align: center;
+    color:navy;
+    font-weight: "bold"; 
+   }
+
+div.gallery {
+  margin: 10px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 300px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width:300px;
+  height:350px;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+
+</style>
+</head>
+<body>
+<h2>Welocme to E-Books website</h2>
+<h3>Download CSE branch books for free</h3>
+<div class="gallery">
+  
+<a href="cse/c1.pdf">
+    <img alt="" src="cse/c1.jpg">
+  </a>
+  
+</div>
+
+<div class="gallery">
+   
+<a href="cse/c2.pdf">
+    <img alt="" src="cse/c2.jpg">
+  </a>
+  
+</div>
+
+<div class="gallery">
+  <a href="cse/c3.pdf">
+    <img alt="" src="cse/c3.jpg">
+  </a>
+
+</div>
+
+
+<div class="gallery">
+  <a href="cse/c4.pdf">
+    <img alt="" src="cse/l1.jpg">
+  </a>
+
+</div>
+
+
+
+
+
+<div class="gallery">
+  <a href="cse/c5.pdf">
+    <img alt="" src="cse/l2.jpg">
+  </a>
+ 
+</div>
+
+
+
+
+<div class="gallery">
+  <a href="cse/c6.pdf">
+    <img alt="" src="cse/m1.jpeg">
+  </a>
+ 
+</div>
+
+
+
+
+<div class="gallery">
+  <a href="cse/c7.pdf">
+    <img alt="" src="cse/p1.jpg">
+  </a>
+  
+</div>
+
+
+
+
+<div class="gallery">
+  <a href="cse/c8.pdf">
+    <img alt="" src="cse/p2.jpg">
+  </a>
+  
+</div>
+
+</body>
+</html>
